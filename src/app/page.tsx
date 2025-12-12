51b685de0f36ca1e893522b7f05483fe327420a1
BhaumikKhandela <152811705+BhaumikKhandela@users.noreply.github.com>
2025-12-13 00:13:06 +0530

    Setup Inngest for background jobs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,23 @@
 
 import { Button } from "@/components/ui/button";
 import { authClient } from "@/lib/auth-client";
+import { useTRPC } from "@/trpc/client";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 
 export default function Page() {
-  const { data } = authClient.useSession();
+  const trpc = useTRPC();
+  const queryClient = useQueryClient();
+  const { data } = useQuery(trpc.getWorkflows.queryOptions());
+
+  const create = useMutation(trpc.createWorkflow.mutationOptions({
+    onSuccess: () => {
+      queryClient.invalidateQueries(trpc.getWorkflows.queryOptions());
+    }
+  }));
   return (
     <div className="min-h-screen min-w-screen flex items-center justify-center">
       {JSON.stringify(data)}
-      {data && <Button onClick={() => authClient.signOut}>Logout</Button>}
+      <Button disabled={create.isPending} onClick={() => { create.mutate()}}></Button>
     </div>
   );
 }
