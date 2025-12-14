6f21ffe93ac6c26f6b6210c0f59cf0686e090cb9
BhaumikKhandela <152811705+BhaumikKhandela@users.noreply.github.com>
2025-12-14 22:29:35 +0530

    Checkpoint

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,6 +1,8 @@
 import { z } from "zod";
 import prisma from "../../lib/db";
 import { createTRPCRouter, protectedProcedure } from "../init";
+import { google } from "@ai-sdk/google";
+import { generateText } from "ai";
 export const appRouter = createTRPCRouter({
   getWorkflows: protectedProcedure.query(({ ctx }) => {
     return prisma.workflow.findMany();
@@ -12,6 +14,15 @@ export const appRouter = createTRPCRouter({
         name: "test-workflow",
       },
     });
+  }),
+
+  testAi: protectedProcedure.mutation(async () => {
+    const { text } = await generateText({
+      model: google("gemini-2.5-flash"),
+      prompt: "Write a vegetarian lasgna recipe for 4 people",
+    });
+
+    return text;
   }),
 });
 // export type definition of API
