77462daa3cb21ab56b51174025c904b2db7caa1d
BhaumikKhandela <152811705+BhaumikKhandela@users.noreply.github.com>
2025-12-16 14:05:40 +0530

    Payment Integration

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -2,7 +2,7 @@ import { SignUpForm } from "@/features/auth/components/register-form";
 import { requireUnauth } from "@/lib/auth-utils";
 
 const Page = async () => {
-  await requireUnauth();
+
   return <SignUpForm />;
 };
 
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,10 +1,39 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import prisma from "./db";
+import {
+  polar,
+  checkout,
+  portal,
+  usage,
+  webhooks,
+} from "@polar-sh/better-auth";
+import { Polar } from "@polar-sh/sdk";
+import { polarClient } from "./polar";
+
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: "postgresql",
   }),
+  plugins: [
+    polar({
+      client: polarClient,
+      createCustomerOnSignUp: true,
+      use: [
+        checkout({
+          products: [
+            {
+              productId: "c130278c-b1b2-4de7-9245-6a144de62135",
+              slug: "Athena-Pro", // Custom slug for easy reference in Checkout URL, e.g. /checkout/Athena-Pro
+            },
+          ],
+          successUrl: process.env.POLAR_SUCCESS_URL,
+          authenticatedUsersOnly: true,
+        }),
+        portal(),
+      ],
+    }),
+  ],
   emailAndPassword: {
     enabled: true,
     autoSignIn: true,
