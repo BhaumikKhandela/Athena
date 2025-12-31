1670bbfba33b4746b2da5b5cb8396b9737c3162c
BhaumikKhandela <152811705+BhaumikKhandela@users.noreply.github.com>
2025-12-31 23:48:58 +0530

    feat: implement credential management system with CRUD operations and UI components

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,7 +1,9 @@
+import { credentialsRouter } from "@/features/credentials/server/routers";
 import { createTRPCRouter, protectedProcedure } from "../init";
 import { workflowsRouter } from "@/features/workflows/server/routers";
 export const appRouter = createTRPCRouter({
   workflows: workflowsRouter,
+  credentials: credentialsRouter,
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
